e155c7061bc861754bb3f48f8cca1cd5f58143cb
kenstufsz <stanleyekene.ze@gmail.com>
2020-01-16 00:56:52 -0600

    Updates

---
+++ precache-manifest.1db1f182077e79f9e9a77b013b9bc848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7fac08d1ae23030700dabdc43ee437",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "dc48ebfec595daaabdcf",
+    "url": "/test2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c2a63fc0eb8c23200d3",
+    "url": "/test2/static/js/2.fbe97a9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test2/static/js/2.fbe97a9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc48ebfec595daaabdcf",
+    "url": "/test2/static/js/main.ce03bda0.chunk.js"
+  },
+  {
+    "revision": "f4f51fa85849fdcc3728",
+    "url": "/test2/static/js/runtime-main.5403fc1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test2/static/media/logo.5d5d9eef.svg"
+  }
+]);
